a61d53102356ea39820e35528639e20259949f69
hhhhh <52508643+gggvik@users.noreply.github.com>
2023-04-07 12:39:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,8 +7,13 @@ from scipy.stats import norm
 chat_id = 250483508 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    n = x.shape[0]
-    left = np.quantile(x, (1 - p) / 2)
-    right = np.quantile(x, 1 - (1 - p) / 2)
-    return (left, right)
+    left_bound = 0.077
+    max_x = max(x)
+    n = len(x)
 
+    g1 = (1 - p) ** (1 / n)
+    left = max_x
+    right = (max_x - left_bound) / g1 + left_bound
+    magic_size_add = (right - left) * 0.3
+
+    return (left, right + magic_size_add)
